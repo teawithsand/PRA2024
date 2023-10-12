56d3ae2b9b2627f568a5fd6487c513ddabb65fd3
teawithsand <teawithsand@gmail.com>
2023-10-12 10:48:06 +0200

    Init commit; initialized logger

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
 public class HelloWorld {
 
+    private static Logger LOG = Logger.getLogger("name");
+
     public static void main(String [ ] args) {
-        System.out.println("Hello! Witaj!");
+        LOG.log(Priority.WARN, "asdf");
     }
 }
